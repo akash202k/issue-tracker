503cf67afa082b6c33b6eda46d5e7adc6b9a7de9
Akash Pawar <akashpawar202k@gmail.com>
2024-03-08 18:22:49 +0530

    radix-ui set

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const Issues = () => {
     return (
-        <div className='p-5'>Issues Page</div>
+        <div className='p-5'><Button>New Issue</Button></div>
     )
 }
 
