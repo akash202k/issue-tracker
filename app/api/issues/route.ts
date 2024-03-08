9e6ec25061218327c949f261381e41273cad52e6
Akash Pawar <akashpawar202k@gmail.com>
2024-03-08 23:21:48 +0530

    error message

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: NextRequest) {
     const validation = Schema.issueSchema.safeParse(body);
     if (!validation.success) {
         console.log(validation.error.errors);
-        return NextResponse.json(validation.error.errors, { status: 400 })
+        return NextResponse.json(validation.error.format(), { status: 400 })
     }
 
     const newIssue = await prisma.issue.create({
@@ -15,8 +15,7 @@ export async function POST(req: NextRequest) {
             title: body.title,
             description: body.description
         }
-
     })
 
-    return NextResponse.json("issue created", { status: 200 });
+    return NextResponse.json({ message: "New Ticket Created" }, { status: 200 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,90 @@
 "use client"
 
 import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form'
+import Schema from "../../api/zod"
+import { z } from "zod"
+import axios from 'axios';
+import { zodResolver } from "@hookform/resolvers/zod"
+
+
 
 
 const NewIssuePage = () => {
+
+    type IssueFormData = {
+        title: String,
+        description: String
+    }
+
+    // type IssueFormData = z.infer<typeof Schema.issueSchema>
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+
+    } = useForm<IssueFormData>();
+
+    const router = useRouter();
+
+    const onSubmit = handleSubmit(async (data) => {
+        // console.log(data);
+        try {
+            const createTicket = await axios.post('/api/issues',
+                {
+                    title: data.title,
+                    description: data.description
+                })
+
+            router.push('/issues');
+        } catch (error: any) {
+            // console.log(error.messages);
+            // console.log(error?.response?.data?.title?._errors[0])
+            // console.log(error?.response?.data?.description?._errors[0])
+            setTitleError(error?.response?.data?.title?._errors[0]);
+            setDescriptionError(error?.response?.data?.description?._errors[0]);
+
+        }
+
+
+
+    }
+    )
+
+    const [titleError, setTitleError] = useState("");
+    const [descriptionError, setDescriptionError] = useState("");
+
+
+
+
     return (
-        <div className='max-w-xl space-y-4'>
-            <TextField.Root>
-                <TextField.Input placeholder='Title' />
+        <div>
+            {/* <div>{error ? error.}</div> */}
+            <form onSubmit={onSubmit} className=' flex p-5  bg-gray-100 rounded-md h-screen justify-center '>
+                <div className='flex flex-col  justify-center '>
+                    <label className='text-3xl pb-4 font-bold'>Create New Ticket</label>
+                    <div className='space-y-5 h-max w-80 mx-auto '>
 
-            </TextField.Root>
-            <SimpleMDE placeholder="Description" />
-            <Button >New Ticket</Button>
-
+                        <div >
+                            {titleError ? <label className='text-red-600 text-sm'>{titleError}</label> : ""}
+                            <input  {...register('title')} className='text-left w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500' type="text" placeholder='Title' />
+                        </div>
+                        <div>
+                            {descriptionError ? <label className=' text-red-600 text-sm'>{descriptionError}</label> : ""}
+                            <input {...register('description')} className=" text-left w-full h-24 p-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500" placeholder='Description' ></input>
+                        </div>
+                        <button className='w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>New Ticket</button>
+                    </div>
+                </div>
+            </form>
         </div>
+
+
     )
 }
 
